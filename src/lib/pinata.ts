dc4537139685257202c4972e5d38bd2128a33e99
App <app042731@gmail.com>
2025-10-30 22:17:56 +0000

    Try fixing this error: `Runtime Error: Error: Failed to execute 'append'

--- src/lib/pinata.ts
+++ src/lib/pinata.ts
@@ -1,44 +1,32 @@
 import axios from 'axios';
-import { Readable } from 'stream';
 
 const PINATA_GATEWAY_URL = process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL;
 const PINATA_JWT = process.env.NEXT_PUBLIC_PINATA_JWT;
 
-async function uploadToPinata(
-  fileStream: Readable,
-  fileName: string
-): Promise<string> {
-  if (!PINATA_GATEWAY_URL || !PINATA_JWT) {
-    throw new Error(
-      'Pinata environment variables are not set up correctly on the server.'
-    );
-  }
-
-  const data = new FormData();
-  data.append('file', fileStream as any, fileName);
-
-  try {
-    const res = await axios.post(
-      'https://api.pinata.cloud/pinning/pinFileToIPFS',
-      data,
-      {
-        headers: {
-          'Content-Type': `multipart/form-data; boundary=${data.getBoundary()}`,
-          Authorization: `Bearer ${PINATA_JWT}`,
-        },
-      }
-    );
-    return `${PINATA_GATEWAY_URL}/ipfs/${res.data.IpfsHash}`;
-  } catch (error) {
-    console.error('Error uploading file to Pinata:', error);
-    throw new Error(`Error uploading file to Pinata: ${error}`);
-  }
-}
-
 export async function uploadFile(file: File): Promise<string> {
-  const arrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  const stream = Readable.from(buffer);
+    if (!PINATA_GATEWAY_URL || !PINATA_JWT) {
+        throw new Error(
+        'Pinata environment variables are not set up correctly.'
+        );
+    }
 
-  return uploadToPinata(stream, file.name);
+    const data = new FormData();
+    data.append('file', file);
+
+    try {
+        const res = await axios.post(
+            'https://api.pinata.cloud/pinning/pinFileToIPFS',
+            data,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${PINATA_JWT}`,
+                },
+            }
+        );
+        return `${PINATA_GATEWAY_URL}/ipfs/${res.data.IpfsHash}`;
+    } catch (error) {
+        console.error('Error uploading file to Pinata:', error);
+        throw new Error(`Error uploading file to Pinata: ${error}`);
+    }
 }
