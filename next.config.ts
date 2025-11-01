55b4c828c6ef5b28ca52bf34038d3f223cee5bd8
App <app042731@gmail.com>
2025-11-01 16:13:50 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https"
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
@@ -44,6 +44,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'archive.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.mypinata.cloud',
         port: '',
         pathname: '/**',
       }
